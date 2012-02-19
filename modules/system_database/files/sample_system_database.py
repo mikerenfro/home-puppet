2b676a2a860a489564e588e4eab447addbefd410
Mike Renfro <mike+github@renf.ro>
2012-02-19 14:07:06 -0600

    preparing to add system_database module

---
+++ modules/system_database/files/sample_system_database.py
@@ -0,0 +1,5 @@
+# Modify entries below, then rename this to system_database.py before
+# using. Format: (hostname, mac, ip)
+SYSTEM_DATABASE = [
+    ('host1', '00:00:00:00:00:00', '10.0.0.1'),
+    ]
